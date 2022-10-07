67708d08b834670da4cdbd0d2b76a548b6b6e083
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-07 13:48:13 +0300

    added class SignUp and sorted imports

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# users/views.py
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+# Импортируем класс формы, чтобы сослаться на неё во view-классе
+from .forms import CreationForm
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    # После успешной регистрации перенаправляем пользователя на главную.
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
