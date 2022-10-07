b15cd5488240aa86a1d691502f33f5ca6dc6b2ed
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-07 14:19:35 +0300

    added path signup

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -3,14 +3,21 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
+from . import views
+
 app_name = 'users'
 
 urlpatterns = [
     path(
-      'logout/',
-      # Прямо в описании обработчика укажем шаблон, 
-      # который должен применяться для отображения возвращаемой страницы.
-      LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
+        'logout/',
+        # Прямо в описании обработчика укажем шаблон,
+        # который должен применяться для отображения возвращаемой страницы.
+        LogoutView.as_view(template_name='users/logged_out.html'),
+        name='logout'
+    ),
+    path(
+        'signup/',
+        views.SignUp.as_view(),
+        name='signup'
     ),
 ]
