e2485f44cf43b3fd588e7b222fdf6aee7bc7f388
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-08 12:04:22 +0300

    views и urls.py about

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# about/views.py
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+# Описать класс AboutAuthorView для страницы about/author
+class AboutAuthorView(TemplateView):
+    template_name = 'about/author.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Здесь можно произвести какие-то действия для создания контекста.
+
+        return context
+
+# Описать класс AboutTechView для страницы about/tech
+class AboutTechView(TemplateView):
+    template_name = 'about/tech.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Здесь можно произвести какие-то действия для создания контекста.
+
+        return context 
