3269dd4af737e08f7acd5020105b46dac9ab601b
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-08 16:52:33 +0300

    login => pw reset

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -35,41 +35,41 @@ urlpatterns = [
     # Смена пароля
     path(
         'password_change/',
-        PasswordChangeView.as_view('users/password_change_form.html'),
+        PasswordChangeView.as_view(template_name='users/password_change_form.html'),
         name='password_change'
     ),
 
     # Сообщение об успешном изменении пароля
     path(
         'password_change/done/',
-        PasswordChangeDoneView.as_view('users/password_change_done.html'),
+        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
         name='password_change_done'
     ),
 
     # Восстановление пароля
     path(
         'password_reset/',
-        PasswordResetView.as_view('users/password_reset_form.html'),
+        PasswordResetView.as_view(template_name='users/password_reset_form.html'),
         name='password_reset'
     ),
 
     # Сообщение об отправке ссылки для восстановления пароля
     path(
         'password_reset/done/',
-        PasswordResetDoneView.as_view('users/password_reset_done.html'),
+        PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
         name='password_reset_done'
     ),
 
     # Вход по ссылке для восстановления пароля
     path(
         'reset/<uidb64>/<token>/',
-        PasswordResetConfirmView.as_view('users/password_reset_confirm.html'),
+        PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
         name='password_reset_confirm'
         ),
 
     # Сообщение об успешном восстановлении пароля
     path('reset/done/',
-    PasswordResetCompleteView.as_view('users/password_reset_complete.html'),
+    PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
     name='password_reset_complete'
     ),
 ]
