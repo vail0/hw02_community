e7f1d05f40ff4fd2c022b1cce651612b4718946d
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-08 16:00:40 +0300

    eddited to add new templates

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -25,4 +25,45 @@ urlpatterns = [
         LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
+  
+    # Смена пароля
+    path(
+        'password_change/',
+        views.PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+
+    # Сообщение об успешном изменении пароля
+    path(
+        'password_change/done/',
+        views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    ),
+
+    # Восстановление пароля
+    path(
+        'password_reset/',
+        views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+
+    # Сообщение об отправке ссылки для восстановления пароля
+    path(
+        'password_reset/done/',
+        views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+
+    # Вход по ссылке для восстановления пароля
+    path(
+        'reset/<uidb64>/<token>/',
+        views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+        ),
+
+    # Сообщение об успешном восстановлении пароля
+    path('reset/done/',
+    views.PasswordResetCompleteView.as_view(),
+    name='password_reset_complete'
+    ),
 ]
