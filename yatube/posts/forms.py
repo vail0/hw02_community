b060c8e36bb1dde844ad1d0d3304bd914ac25e5b
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-09 22:31:43 +0300

    create update

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -7,3 +7,15 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group')
+    
+    # Метод-валидатор для поля text
+    def clean_subject(self):
+        data = self.cleaned_data['text']
+
+        # Если пользователь не поблагодарил администратора - считаем это ошибкой
+        if data == '':
+            raise forms.ValidationError('Поле пустое')
+
+        # Метод-валидатор обязательно должен вернуть очищенные данные, 
+        # даже если не изменил их
+        return data 
