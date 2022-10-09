c63cccee96916c1fa1f6667dd8227cc6444cc39f
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-09 16:59:58 +0300

    profile and post_detail

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -35,27 +35,27 @@ def profile(request, username):
     # Здесь код запроса к модели и создание словаря контекста
     author = get_object_or_404(User, username=username)
     posts_auth = author.posts.all()
+
     paginator = Paginator(posts_auth, 10)
     # Из URL извлекаем номер запрошенной страницы - это значение параметра page
     page_number = request.GET.get('page')
     # Получаем набор записей для страницы с запрошенным номером
-    page_auth = paginator.get_page(page_number)
+    page_obj = paginator.get_page(page_number)
     context = {
         'author':author,
         'posts_auth':posts_auth,
-        'page_obj':page_auth,
-        # 'page_auth' не видит paginator
+        'page_obj':page_obj,
     }
     return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
     # Здесь код запроса к модели и создание словаря контекста
-    post = get_object_or_404(Post, id=post_id)
-    post_30 = post.posts.all()
-    
+    post = get_object_or_404(Post, pk=post_id)
+    posts_by = post.author.posts.all()    
     context = {
-        'post_30':post_30,
+        'post':post,
+        'posts_by':posts_by,
 
     }
     return render(request, 'posts/post_detail.html', context)
