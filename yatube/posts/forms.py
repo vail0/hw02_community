264a22ac622ade255624f9a254d3bbf5da6f5704
vail0 <97312541+vail0@users.noreply.github.com>
2022-10-10 22:03:28 +0300

    deletet comments

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -4,21 +4,7 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
-    # text = forms.CharField()
-    # group = forms.ChoiceField(required=False)
 
     class Meta:
         model = Post
         fields = ('text', 'group')
-
-    # Метод-валидатор для поля text
-    # def clean_text(self):
-    #     data = self.cleaned_data['text']
-
-    #     # Если пользователь не заполнил поле - считаем это ошибкой
-    #     if data == '':
-    #         raise forms.ValidationError('Поле пустое')
-
-    #     # Метод-валидатор обязательно должен вернуть очищенные данные,
-    #     # даже если не изменил их
-    #     return data
